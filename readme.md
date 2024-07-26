d31277cfeb64e6dc354fff4b5d0e3f80d962f414
Ali Raza <kongd4669@gmail.com>
2024-07-27 00:14:57 +0500

    some relocation of code

--- readme.md
+++ readme.md
@@ -8,3 +8,11 @@
           <span class="footer-inside">Contact Us</span>
         </div>
         
+      
+
+      1.website enter
+      2.translate =100%==>{which means the nav bar is out of screen}
+      3.clicks the toggle button
+      4.event listener triggered 
+      5.translate goes to 0% ==> {which means it has 80vw }
+      5.1 event listener is in if mode as istranslated was false, it was trigerred
--- vite-project/main.js
+++ vite-project/main.js
@@ -1,4 +1,8 @@
 const arrows  = document.querySelectorAll(".arrow-imgs")
+const navBarToggleBtn = document.querySelector("#nav-bar-icon")
+const asideNavOpen = document.querySelector(".nav-container-aside")
+const asideSemantic =document.querySelector(".nav-aside-open")
+const backdropFilter =document.querySelector(".backdrop")
 
 
 {
@@ -15,4 +19,26 @@ arrows.forEach((upArrow)=>{
         }
     })
 })
+}
+
+
+{
+    let isTranslated = true; //translate is at 0% by default which means that nav-bar is hidden .... also the value of isTranslated is taken as 100% which means that the bar is hidden. 
+    navBarToggleBtn.addEventListener("click",()=>{
+        if(isTranslated==true){
+            navBarToggleBtn.style.transform = "rotate(90deg)"
+            asideNavOpen.style.transform = "translate(0%)";
+            document.body.style = "overflow-y:hidden";
+           
+            isTranslated=false;
+        }else{
+            
+            navBarToggleBtn.style.transform = "rotate(0deg)"
+            asideNavOpen.style.transform = "translate(100%)"
+            document.body.style = "overflow-y:visible";
+            
+            isTranslated=true
+        }
+    })
+    
 }
